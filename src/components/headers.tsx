4811ce6fa7677ad4d027ec258e372e107736df71
dpak1999 <deepakkumardash313@gmail.com>
2021-02-27 14:59:47 +0530

    add : headers & check types for email

--- src/components/headers.tsx
+++ src/components/headers.tsx
@@ -11,15 +11,22 @@ export const Header = () => {
   const { data } = useMe();
 
   return (
-    <header className="py-4">
-      <div className="w-full px-5 xl:px-0 max-w-screen-xl mx-auto flex justify-between items-center">
-        <img src={deatsLogo} alt="LOGO" className="w-24" />
-        <span className="text-sm">
-          <Link to="/my-profile">
-            <FontAwesomeIcon icon={faUser} className="text-xl" />
-          </Link>
-        </span>
-      </div>
-    </header>
+    <>
+      {!data?.me.verified && (
+        <div className="bg-red-500 p-3 text-center px-3 text-sm text-white">
+          <span>Please verify your email</span>
+        </div>
+      )}
+      <header className="py-4">
+        <div className="w-full px-5 xl:px-0 max-w-screen-xl mx-auto flex justify-between items-center">
+          <img src={deatsLogo} alt="LOGO" className="w-24" />
+          <span className="text-sm">
+            <Link to="/my-profile">
+              <FontAwesomeIcon icon={faUser} className="text-xl" />
+            </Link>
+          </span>
+        </div>
+      </header>
+    </>
   );
 };
