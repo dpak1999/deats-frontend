6b834ceb5a55a54ab553dd7a30acee00dccda0a7
dpak1999 <deepakkumardash313@gmail.com>
2021-03-02 16:12:45 +0530

    add : edit profile mutation

--- src/pages/user/edit-profile.tsx
+++ src/pages/user/edit-profile.tsx
@@ -1,18 +1,104 @@
 /** @format */
 
+import { gql, useApolloClient, useMutation } from "@apollo/client";
 import React from "react";
+import { useForm } from "react-hook-form";
 import { Button } from "../../components/button";
 import { useMe } from "../../hooks/useMe";
+import {
+  editProfile,
+  editProfileVariables,
+} from "../../__generated__/editProfile";
+
+const EDIT_PROFILE_MUTATION = gql`
+  mutation editProfile($input: EditProfileInput!) {
+    editProfile(input: $input) {
+      ok
+      error
+    }
+  }
+`;
+
+interface IFormProps {
+  email?: string;
+  password?: string;
+}
 
 export const EditProfile = () => {
   const { data: userData } = useMe();
+  const client = useApolloClient();
+  const onCompleted = (data: editProfile) => {
+    const {
+      editProfile: { ok },
+    } = data;
+    if (ok && userData) {
+      const {
+        me: { email: prevEmail, id },
+      } = userData;
+      const { email: newEmail } = getValues();
+      if (prevEmail !== newEmail) {
+        client.writeFragment({
+          id: `User:${id}`,
+          fragment: gql`
+            fragment EditUser on User {
+              verified
+              email
+            }
+          `,
+          data: { email: newEmail, verified: true },
+        });
+      }
+    }
+  };
+  const [editProfile, { loading }] = useMutation<
+    editProfile,
+    editProfileVariables
+  >(EDIT_PROFILE_MUTATION, {
+    onCompleted,
+  });
+  const { register, handleSubmit, getValues, formState } = useForm<IFormProps>({
+    mode: "onChange",
+    defaultValues: {
+      email: userData?.me.email,
+    },
+  });
+  const onSubmit = () => {
+    const { email, password } = getValues();
+    editProfile({
+      variables: {
+        input: {
+          email,
+          ...(password !== "" && { password }),
+        },
+      },
+    });
+  };
   return (
     <div className="mt-52 flex flex-col justify-center items-center">
       <h4 className="font-semibold text-2xl mb-3">Edit profile</h4>
-      <form className="grid gap-3 mt-5 w-full mb-5 max-w-screen-sm">
-        <input className="input" type="email" placeholder="Email" />
-        <input className="input" type="password" placeholder="Password" />
-        <Button loading={false} canClick={true} actionText="Save Profile" />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="grid gap-3 mt-5 w-full mb-5 max-w-screen-sm"
+      >
+        <input
+          ref={register}
+          className="input"
+          name="email"
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          ref={register}
+          className="input"
+          name="password"
+          type="password"
+          placeholder="Password"
+        />
+        <Button
+          loading={loading}
+          canClick={formState.isValid}
+          actionText="Save Profile"
+        />
       </form>
     </div>
   );
