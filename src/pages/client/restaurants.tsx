1eae9401ecd80691a4a51e04b6cc5a800084dbb7
dpak1999 <deepakkumardash313@gmail.com>
2021-03-03 19:08:08 +0530

    add : restaurants query

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -1,7 +1,49 @@
 /** @format */
 
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import {
+  restaurantsPageQuery,
+  restaurantsPageQueryVariables,
+} from "../../__generated__/restaurantsPageQuery";
+
+const RESTAURANTS_QUERY = gql`
+  query restaurantsPageQuery($input: RestaurantsInput!) {
+    allCategories {
+      ok
+      error
+      categories {
+        id
+        name
+        coverImage
+        slug
+        restaurantCount
+      }
+    }
+    restaurants(input: $input) {
+      ok
+      error
+      totalPages
+      totalResults
+      results {
+        id
+        name
+        coverImage
+        category {
+          name
+        }
+        address
+        isPromoted
+      }
+    }
+  }
+`;
 
 export const Restaurants = () => {
+  const { data, loading, error } = useQuery<
+    restaurantsPageQuery,
+    restaurantsPageQueryVariables
+  >(RESTAURANTS_QUERY, { variables: { input: { page: 1 } } });
+
   return <h1>Restaurants</h1>;
 };
