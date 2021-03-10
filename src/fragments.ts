c9c2a3f1508004329895dd705c8f62b8e52e24d0
dpak1999 <deepakkumardash313@gmail.com>
2021-03-11 00:12:12 +0530

    add : restaurant fragment

---
+++ src/fragments.ts
@@ -0,0 +1,16 @@
+/** @format */
+
+import { gql } from "@apollo/client";
+
+export const RESTAURANT_FRAGMENT = gql`
+  fragment RestaurantParts on Restaurant {
+    id
+    name
+    coverImage
+    category {
+      name
+    }
+    address
+    isPromoted
+  }
+`;
