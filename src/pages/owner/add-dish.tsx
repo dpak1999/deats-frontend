8001de417a7d9afc9037c7afca488cbe561de2a9
dpak1999 <deepakkumardash313@gmail.com>
2021-05-04 23:27:04 +0530

    add : create dish form

--- src/pages/owner/add-dish.tsx
+++ src/pages/owner/add-dish.tsx
@@ -1,7 +1,16 @@
 /** @format */
 
-import { gql } from "@apollo/client";
-import { useParams } from "react-router";
+import { gql, useMutation } from "@apollo/client";
+import { useState } from "react";
+import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { useHistory, useParams } from "react-router";
+import { Button } from "../../components/button";
+import {
+  createDish,
+  createDishVariables,
+} from "../../__generated__/createDish";
+import { MY_RESTAURANT_QUERY } from "./my-restaurant";
 
 const CREATE_DISH_MUTATION = gql`
   mutation createDish($input: CreateDishInput!) {
@@ -16,7 +25,134 @@ interface IParams {
   restaurantId: string;
 }
 
+interface IForm {
+  name: string;
+  price: string;
+  description: string;
+}
+
 export const AddDish = () => {
   const { restaurantId } = useParams<IParams>();
-  return <h1>Dish</h1>;
+  const history = useHistory();
+  const [createDishMutation, { loading }] = useMutation<
+    createDish,
+    createDishVariables
+  >(CREATE_DISH_MUTATION, {
+    refetchQueries: [
+      {
+        query: MY_RESTAURANT_QUERY,
+        variables: { input: { id: +restaurantId } },
+      },
+    ],
+  });
+  const {
+    register,
+    handleSubmit,
+    formState,
+    getValues,
+    setValue,
+  } = useForm<IForm>({
+    mode: "onChange",
+  });
+
+  const onSubmit = () => {
+    const { description, name, price, ...rest } = getValues();
+    // createDishMutation({
+    //   variables: {
+    //     input: {
+    //       name,
+    //       price: +price,
+    //       description,
+    //       restaurantId: +restaurantId,
+    //     },
+    //   },
+    // });
+    // history.goBack();
+    console.log(rest);
+  };
+
+  const [optionsNumber, setOptionsNumber] = useState(0);
+
+  const onAddOptionClick = () => {
+    setOptionsNumber((current) => current + 1);
+  };
+
+  const onDeleteClick = (idToDelete: number) => {
+    setOptionsNumber((current) => current - 1);
+    // @ts-ignore
+    setValue(`${idToDelete}-optionName`, "");
+    // @ts-ignore
+    setValue(`${idToDelete}-optionExtra`, "");
+  };
+
+  return (
+    <div className="container flex flex-col items-center mt-52">
+      <Helmet>
+        <title>Add Dish | Deats</title>
+      </Helmet>
+      <h4 className="font-semibold text-2xl mb-3">Add Dish</h4>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="grid max-w-screen-sm gap-3 mt-5 w-full mb-5"
+      >
+        <input
+          type="text"
+          className="input"
+          name="name"
+          placeholder="Name of Dish"
+          ref={register({ required: "Name is required" })}
+        />
+        <input
+          type="number"
+          className="input"
+          name="price"
+          min={0}
+          placeholder="Price"
+          ref={register({ required: "Price is required" })}
+        />
+        <input
+          type="text"
+          className="input"
+          name="description"
+          placeholder="Description"
+          ref={register({ required: "Descripton is required" })}
+        />
+        <div className="my-10">
+          <h4 className="font-medium mb-3 text-lg">Dish Options</h4>
+          <span
+            onClick={onAddOptionClick}
+            className="cursor-pointer text-white bg-gray-900 py-1 px-2 mt-5"
+          >
+            Add Dish Option
+          </span>
+          {optionsNumber !== 0 &&
+            Array.from(new Array(optionsNumber)).map((_, index) => (
+              <div key={index} className="mt-5">
+                <input
+                  ref={register}
+                  name={`${index}-optionName`}
+                  className="py-2 px-4 mr-3 focus:outline-none focus:border-gray-600 border-2"
+                  type="text"
+                  placeholder="Option Name"
+                />
+                <input
+                  ref={register}
+                  name={`${index}-optionExtra`}
+                  className="py-2 px-4 focus:outline-none focus:border-gray-600 border-2"
+                  type="number"
+                  min={0}
+                  placeholder="Price"
+                />
+                <span onClick={() => onDeleteClick(index)}>Delete Option</span>
+              </div>
+            ))}
+        </div>
+        <Button
+          loading={loading}
+          canClick={formState.isValid}
+          actionText="Create Dish"
+        ></Button>
+      </form>
+    </div>
+  );
 };
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -50,7 +50,7 @@ const restaurantRoutes = [
   { path: "/", component: <MyRestaurants /> },
   { path: "/add-restaurant", component: <AddRestaurant /> },
   { path: "/restaurant/:id", component: <MyRestaurant /> },
-  { path: "/restaurant/:id/add-dish", component: <AddDish /> },
+  { path: "/restaurant/:restaurantId/add-dish", component: <AddDish /> },
 ];
 
 const LoggedInRouter = () => {
