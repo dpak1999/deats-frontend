db899f00a3a6741b7f76bbdde12617dd0f67ed58
Deepak kumar dash <deepakkumardash313@gmail.com>
2021-06-23 01:04:12 +0530

    add : dish mutation types and ui

--- src/components/dish.tsx
+++ src/components/dish.tsx
@@ -1,12 +1,15 @@
 /** @format */
 
 import React from "react";
+import { restaurant_restaurant_restaurant_menu_options } from "../__generated__/restaurant";
 
 interface IDishProps {
   description: string;
   name: string;
   price: number;
   photo: string | null;
+  isCustomer?: boolean;
+  options?: restaurant_restaurant_restaurant_menu_options[] | null;
 }
 
 export const Dish: React.FC<IDishProps> = ({
@@ -14,6 +17,8 @@ export const Dish: React.FC<IDishProps> = ({
   description,
   photo,
   price,
+  isCustomer = false,
+  options,
 }) => {
   return (
     <div className="px-8 py-4 border hover:border-gray-800 cursor-pointer transition-all mb6">
@@ -22,11 +27,22 @@ export const Dish: React.FC<IDishProps> = ({
           style={{ backgroundImage: `url(${photo})` }}
           className="py-32 bg-cover mb-3"
         ></div>
-        <div className="mb-5">
+        <div className="mb-3">
           <h3 className="text-lg font-medium">{name}</h3>
           <h4 className="font-medium">{description}</h4>
         </div>
         <span>${price}</span>
+        {isCustomer && options && options?.length !== 0 && (
+          <div>
+            <h5 className="mt-3 mb1 font-medium">Dish options:</h5>
+            {options?.map((option, index) => (
+              <span className="flex items-center" key={index}>
+                <h6 className="mr-2">{option.name}</h6>
+                <h6 className="text-sm opacity-75">(${option.extra})</h6>
+              </span>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -1,13 +1,14 @@
 /** @format */
 
-import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router";
-import { RESTAURANT_FRAGMENT } from "../../fragments";
+import { DISH_FRAGMENT, RESTAURANT_FRAGMENT } from "../../fragments";
 import {
   restaurant,
   restaurantVariables,
 } from "../../__generated__/restaurant";
+import { Dish } from "../../components/dish";
 
 const RESTAURANT_QUERY = gql`
   query restaurant($input: RestaurantInput!) {
@@ -16,10 +17,14 @@ const RESTAURANT_QUERY = gql`
       error
       restaurant {
         ...RestaurantParts
+        menu {
+          ...DishParts
+        }
       }
     }
   }
   ${RESTAURANT_FRAGMENT}
+  ${DISH_FRAGMENT}
 `;
 
 interface IRestaurantParams {
@@ -55,6 +60,20 @@ export const Restaurant = () => {
             {data?.restaurant.restaurant?.address}
           </h6>
         </div>
+      </div>
+
+      <div className="container grid mt-16 gap-x-5 gap-y-10 md:grid-cols-3 mb-14">
+        {data?.restaurant.restaurant?.menu.map((dish, index) => (
+          <Dish
+            key={index}
+            name={dish.name}
+            description={dish.description}
+            price={dish.price}
+            photo={dish.photo}
+            isCustomer={true}
+            options={dish.options}
+          />
+        ))}
       </div>
     </div>
   );
