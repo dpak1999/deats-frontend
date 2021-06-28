fc4688eb4d7683e6d4751b019e76de30a4126d83
Deepak kumar dash <deepakkumardash313@gmail.com>
2021-06-29 00:48:43 +0530

    add : maps container

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,5 +1,21 @@
 /** @format */
+import GoogleMapReact from "google-map-react";
 
 export const Dashboard = () => {
-  return <h1>Dashboard for driver</h1>;
+  return (
+    <div>
+      <div
+        className="overflow-hidden"
+        style={{ width: window.innerWidth, height: "95vh" }}
+      >
+        <GoogleMapReact
+          defaultCenter={{ lat: 59.95, lng: 30.33 }}
+          defaultZoom={20}
+          bootstrapURLKeys={{ key: "AIzaSyBpuPc7v92_lOY6aVoT8pqG-fpthbHN3Bc" }}
+        >
+          <h1>Hello</h1>
+        </GoogleMapReact>
+      </div>
+    </div>
+  );
 };
