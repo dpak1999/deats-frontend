7648df368138ecef0ea653f89575112ab4130e9f
Deepak kumar dash <deepakkumardash313@gmail.com>
2021-06-29 22:42:41 +0530

    add : typescript with google maps

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -8,9 +8,17 @@ interface ICoords {
   lng: number;
 }
 
+interface IDriverProps {
+  lat: number;
+  lng: number;
+  $hover?: any;
+}
+
+const Driver: React.FC<IDriverProps> = () => <div className="text-3xl">🚙</div>;
+
 export const Dashboard = () => {
   const [driverCoords, setDriverCoords] = useState<ICoords>({ lng: 0, lat: 0 });
-  const [map, setMap] = useState<any>();
+  const [map, setMap] = useState<google.maps.Map>();
   const [maps, setMaps] = useState<any>();
 
   //@ts-ignore
@@ -31,14 +39,23 @@ export const Dashboard = () => {
 
   useEffect(() => {
     if (map && maps) {
-      map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+      map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
+      // const geocoder = new google.maps.Geocoder();
+      // geocoder.geocode(
+      //   {
+      //     location: new google.maps.LatLng(driverCoords.lat, driverCoords.lng),
+      //   },
+      //   (results, status) => {
+      //     console.log(status, results);
+      //   }
+      // );
     }
   }, [driverCoords.lat, driverCoords.lng]);
 
   const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
     setMap(map);
     setMaps(maps);
-    map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
   };
 
   return (
@@ -55,14 +72,7 @@ export const Dashboard = () => {
           bootstrapURLKeys={{ key: "AIzaSyBpuPc7v92_lOY6aVoT8pqG-fpthbHN3Bc" }}
           draggable={false}
         >
-          <div
-            className="text-3xl"
-            // @ts-ignore
-            lat={driverCoords.lat}
-            lng={driverCoords.lng}
-          >
-            🚙
-          </div>
+          <Driver lat={driverCoords.lat} lng={driverCoords.lng} />
         </GoogleMapReact>
       </div>
     </div>
